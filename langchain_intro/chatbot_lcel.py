00812db84cf4886616bfc0aec322239960bf9fb5
gilgamesh7 <felidaesrule@yahoo.com>
2024-06-22 18:37:09 +1200

    LCEL with output parser

--- langchain_intro/chatbot_lcel.py
+++ langchain_intro/chatbot_lcel.py
@@ -1,4 +1,6 @@
 import dotenv
+import logging 
+
 from langchain_openai import ChatOpenAI
 from langchain.prompts import (
     PromptTemplate,
@@ -6,9 +8,68 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
+from langchain_core.output_parsers import StrOutputParser
 
 dotenv.load_dotenv()
+
+# Initialise Logger
+logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {message}", style='{')
+logger = logging.getLogger("LLM_RAG_NEO4J")
 
 # 
 # Chains and LangChain Expression Language (LCEL)
 #
+review_system_template_str = """Your job is to use patient
+    reviews to answer questions about their experience at a
+    hospital. Use the following context to answer questions.
+    Be as detailed as possible, but don't make up any information
+    that's not from the context. If you don't know an answer, say
+    you don't know.
+
+    {context}
+    """
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        template=review_system_template_str
+    )
+)
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        template="{question}"
+    )
+)
+
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context","question"],
+    messages=messages
+)
+
+chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+
+review_chain = review_prompt_template | chat_model
+
+#
+# Try it out
+#
+context = "I had a great stay!"
+question = "Did anyone have a positive experience?"
+
+logger.info(f"LCEL : \n {review_chain.invoke({"context":context, "question":question})}")
+
+#
+# Add Output Parser
+#
+output_parser = StrOutputParser()
+
+review_chain = review_prompt_template | chat_model | output_parser
+
+context = "I had a great stay!"
+question = "Did anyone have a positive experience?"
+
+logger.info(f"LCEL with output parser : \n {review_chain.invoke({"context":context, "question":question})}")
