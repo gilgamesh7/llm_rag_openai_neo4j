9d4d0d2aa2437622b608f9863472eb625f4360ef
gilgamesh7 <felidaesrule@yahoo.com>
2024-07-15 19:22:16 +1200

    Dummy function to simulate API call for agent

---
+++ langchain_intro/05_tools.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+def get_current_wait_time(hospital: str)-> int | str:
+    if hospital not in ["A", "B", "C", "D"]:
+        return f"Hospital {hospital} does not exist"
+    
+    # Simulate API Call
+    time.sleep(1)
+
+    return random.randint(0, 10000)
