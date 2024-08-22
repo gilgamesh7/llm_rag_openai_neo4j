1e1cee5cdab43042194792a46dbbcf79449b0efa
gilgamesh7 <felidaesrule@yahoo.com>
2024-08-22 19:42:45 +1200

    Fix issue with connection parameters for Neo4jVector

--- chatbot_api/src/chains/hospital_review_chain.py
+++ chatbot_api/src/chains/hospital_review_chain.py
@@ -1,7 +1,9 @@
+import os
 from dotenv import dotenv_values
 import logging 
 
-from langchain.vectorstores.neo4j_vector import Neo4jVector
+# from langchain.vectorstores.neo4j_vector import Neo4jVector
+from langchain_community.vectorstores.neo4j_vector import Neo4jVector
 from neo4j import GraphDatabase
 from langchain_openai import OpenAIEmbeddings
 
@@ -18,32 +20,36 @@ HOSPITAL_QA_MODEL = config.get("HOSPITAL_QA_MODEL")
 print(f"{HOSPITAL_QA_MODEL=}")
 print(f"{config.get('NEO4J_URI')=}")
 
-driver = GraphDatabase.driver(
-        config.get("NEO4J_URI"), 
-        auth=(config.get("NEO4J_USERNAME"), config.get("NEO4J_PASSWORD"))
-        ) 
+# TEST CONNECTIVITY
+# driver = GraphDatabase.driver(
+#         config.get("NEO4J_URI"), 
+#         auth=(config.get("NEO4J_USERNAME"), 
+#               config.get("NEO4J_PASSWORD"))
+#         ) 
 
-with driver.session(database="neo4j") as session:
-    print(driver.verify_connectivity())
-    query = f"""MATCH (p:Patient)
-                RETURN p LIMIT 5;"""
+# with driver.session(database="neo4j") as session:
+#     print(driver.verify_connectivity())
+#     query = f"""MATCH (p:Patient)
+#                 RETURN p LIMIT 5;"""
+#     summary = driver.execute_query(query)
+#     print(summary)
     
 # Create Chain
-# neo4j_vector_index = Neo4jVector.from_existing_graph(
-#     embedding=OpenAIEmbeddings(),
-#     url=config.get("NEO4J_URI"),
-#     username=config.get("NEO4J_USERNAME"),
-#     password=config.get("NEO4J_PASSWORD"),
-#     index_name="reviews",
-#     node_label="Review",
-#     text_node_properties=[
-#         "physician_name",
-#         "patient_name",
-#         "text",
-#         "hospital_name",
-#     ],
-#     embedding_node_property="embedding",
-# )
+os.environ["NEO4J_URI"] = config.get("NEO4J_URI")
+os.environ["NEO4J_USERNAME"] = config.get("NEO4J_USERNAME")
+os.environ["NEO4J_PASSWORD"] = config.get("NEO4J_PASSWORD")
+neo4j_vector_index = Neo4jVector.from_existing_graph(
+    embedding=OpenAIEmbeddings(),
+    index_name="reviews",
+    node_label="Review",
+    text_node_properties=[
+        "physician_name",
+        "patient_name",
+        "text",
+        "hospital_name",
+    ],
+    embedding_node_property="embedding",
+)
 
 
 
