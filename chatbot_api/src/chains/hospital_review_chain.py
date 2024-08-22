603d725978fc5fe72b3c038fefb6852794ce745c
gilgamesh7 <felidaesrule@yahoo.com>
2024-08-22 20:05:41 +1200

    Create embeddings & prompts for hospital review chain

--- chatbot_api/src/chains/hospital_review_chain.py
+++ chatbot_api/src/chains/hospital_review_chain.py
@@ -2,10 +2,18 @@ import os
 from dotenv import dotenv_values
 import logging 
 
-# from langchain.vectorstores.neo4j_vector import Neo4jVector
-from langchain_community.vectorstores.neo4j_vector import Neo4jVector
-from neo4j import GraphDatabase
-from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.neo4j_vector import Neo4jVector
+from langchain_openai import (
+    OpenAIEmbeddings,
+    ChatOpenAI
+)
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
+from langchain.chains import RetrievalQA
 
 # Initialise Logger
 logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {message}", style='{')
@@ -21,6 +29,7 @@ print(f"{HOSPITAL_QA_MODEL=}")
 print(f"{config.get('NEO4J_URI')=}")
 
 # TEST CONNECTIVITY
+# from neo4j import GraphDatabase 
 # driver = GraphDatabase.driver(
 #         config.get("NEO4J_URI"), 
 #         auth=(config.get("NEO4J_USERNAME"), 
@@ -39,10 +48,10 @@ os.environ["NEO4J_URI"] = config.get("NEO4J_URI")
 os.environ["NEO4J_USERNAME"] = config.get("NEO4J_USERNAME")
 os.environ["NEO4J_PASSWORD"] = config.get("NEO4J_PASSWORD")
 neo4j_vector_index = Neo4jVector.from_existing_graph(
-    embedding=OpenAIEmbeddings(),
+    embedding=OpenAIEmbeddings(), # The model used to create the embeddings
     index_name="reviews",
-    node_label="Review",
-    text_node_properties=[
+    node_label="Review", # Node to create embeddings for
+    text_node_properties=[ # node properties to include in the embedding
         "physician_name",
         "patient_name",
         "text",
@@ -50,6 +59,47 @@ neo4j_vector_index = Neo4jVector.from_existing_graph(
     ],
     embedding_node_property="embedding",
 )
+
+# Create Prompt template
+review_template = """Your job is to use patient
+reviews to answer questions about their experience at a hospital. Use
+the following context to answer questions. Be as detailed as possible, but
+don't make up any information that's not from the context. If you don't know
+an answer, say you don't know.
+{context}
+"""
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        template=review_template
+        )
+    )   
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        template="{question}"
+        )
+) 
+
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context","question"],
+    messages=messages
+)
+
+
+reviews_vector_chain = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(model_name=HOSPITAL_QA_MODEL, temperature=0), 
+    chain_type="stuff", 
+    retriever=neo4j_vector_index.as_retriever(k=12),
+    return_source_documents=True
+    )
+
+reviews_vector_chain.combine_documents_chain.llm_chain.prompt = review_prompt_template
+
 
 
 
